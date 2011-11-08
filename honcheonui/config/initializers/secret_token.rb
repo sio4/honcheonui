c304d8881e63d675bbf959ae1f3e35c71a5c54f8
Yonghwan SO <sio4@users.sf.net>
2011-11-09 01:19:52 +0900

    $ rails new honcheonui --skip-bundle

---
+++ honcheonui/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Honcheonui::Application.config.secret_token = '773db5dc1ee890446daf2c12d2e27b86d558b97cce539c724c86148331c822261d4bfbdeeda68655d087d5c2049f3df42d267195f719e8ebd66dc5e61b31196d'
