6ea537ee44fef70b4ed0e9ecb931da0bd2a069d9
Yonghwan SO <sio4@users.sf.net>
2011-12-12 18:01:35 +0900

    just cleanup.

--- agent/kyeong.py
+++ agent/kyeong.py
@@ -1,62 +1,40 @@
 #!/usr/bin/env python3
-#
-#
-
-
+"""agent for honcheonui.
+"""
 
 import sys, os
 import time
 
+NAME	= 'kyeong'
+VERSION	= '0.1.1'
+
 import util
 import honcheonui
 
-NAME = 'agent'
-VERSION = '0.1.0'
+###
+### function definitions	----------------------------------------------
+###
 
-### command line configurations	----------------------------------------------
-from optparse import OptionParser
-parser = OptionParser()
-parser.add_option("-c", "--conf", dest="config",
-		default="/etc/opt/honcheonui/honcheonui.xml",
-		help="configuration file", metavar="FILE")
-parser.add_option("-d", "--debug", dest="debug",
-		action="store_true",
-		help="running in foreground")
-parser.add_option("-f", "--force", dest="force",
-		action="store_true",
-		help="kill existing agent and run new one.")
-(options, args) = parser.parse_args()
-
-
-### basic configuration.	----------------------------------------------
-log = util.Log('honcheonui-kyeong')
-log.info("initializing...")
-try:
-	cf = util.Config(options.config)
-except util.configError as e:
-	log.fatal('%s' % e)
-
-# override some configuration (agent mode).
-cf.set('honcheonui/name', 'honcheonui-kyeong')
-cf.set('honcheonui/version', '0.1')
-
-log.set_level(cf.get('honcheonui/loglevel'))
-log.info('%s configured properly...' % cf.get('honcheonui/name'))
-
-# XXX reachable test required!
-if cf.get('master/host') == '':
-	log.fatal("server not configured properly.", os.EX_CONFIG)
-
+def get_options():
+	from optparse import OptionParser
+	parser = OptionParser()
+	parser.add_option("-c", "--conf", dest="config",
+			default="/etc/opt/honcheonui/honcheonui.xml",
+			help="configuration file", metavar="FILE")
+	parser.add_option("-d", "--debug", dest="debug",
+			action="store_true",
+			help="running in foreground")
+	parser.add_option("-f", "--force", dest="force",
+			action="store_true",
+			help="kill existing agent and run new one.")
+	(options, args) = parser.parse_args()
+	return options
 
 ### signal handler and helper functions	--------------------------------------
-import signal
 def sighandler(signum, frame):
 	log.warn("signal %d received." % signum)
 	return
 
-signal.signal(signal.SIGHUP, sighandler)
-
-import atexit
 def saygoodbye():
 	pidlocked = util.read_pidlock(cf.get('honcheonui/pid_file'))
 	if os.getpid() == int(pidlocked):
@@ -65,6 +43,60 @@ def saygoodbye():
 	log.info("bye...")
 	return
 
+### module handlers	------------------------------------------------------
+def run_blocked_modules(path):
+	for m in cf.subkeys(path):
+		log.verb("config '%s' detected. trying to load..." % m)
+		try:
+			mod = __import__('modules.%s' % m, fromlist=["modules"])
+		except ImportError as e:
+			log.error("cannot import module '%s'. ignore." % m)
+		else:
+			log.info("ok, loaded. starting module '%s'..." % m)
+			mod.run(cf, comm, log)
+	return
+
+def run_threaded_modules(path):
+	modules = list()
+	for m in cf.subkeys(path):
+		log.verb("config '%s' detected. trying to load..." % m)
+		try:
+			mod = __import__('modules.%s' % m, fromlist=["modules"])
+			mclass = getattr(mod, m)
+			t = mclass(cf, comm, log, message)
+			t.start()
+		except (ImportError,AttributeError) as e:
+			log.error("cannot import module '%s': %s" % (m, str(e)))
+		except:
+			log.error('unknown exception.')
+			raise
+		else:
+			modules.append(t)
+			log.info("module '%s' registered. starting..." % m)
+	return modules
+
+###
+### start main routine	------------------------------------------------------
+###
+options = get_options()
+
+log = util.Log('honcheonui-kyeong')
+log.info("initializing...")
+try:
+	cf = util.Config(options.config)
+except util.configError as e:
+	log.fatal('%s' % e)
+
+# override some configuration (agent mode).
+cf.set('honcheonui/name', 'honcheonui-%s' % NAME)
+cf.set('honcheonui/version', '0.1')
+
+log.set_level(cf.get('honcheonui/loglevel'))
+log.info('%s configured properly...' % cf.get('honcheonui/name'))
+
+# XXX reachable test required!
+if cf.get('master/host') == '':
+	log.fatal("server not configured properly.", os.EX_CONFIG)
 
 ### go background!	------------------------------------------------------
 if options.debug != True:
@@ -82,10 +114,11 @@ if os.path.exists(cf.get('honcheonui/pid_file')):
 		log.fatal("execution aborted. use --force for ignore it.")
 
 util.write_pidlock(cf.get('honcheonui/pid_file'))
-log.info("%s (%d) started on %s..." % \
-		(cf.get('honcheonui/name'), os.getpid(), os.uname()[1]))
+log.info("%s (%d) started..." % (NAME, os.getpid()))
 
 # yes, about to do my job!
+import signal, atexit
+signal.signal(signal.SIGHUP, sighandler)
 atexit.register(saygoodbye)
 
 
@@ -97,64 +130,28 @@ comm = honcheonui.Communication(cf.get('master/host'), cf.get('master/port'))
 # now use xml config for server basis, but it will be site-overide mode.
 # we need autoconfig via network broadcast.
 
-###
 ### module detection and serve...	--------------------------------------
-###
-
 ### modules for startup time...	----------------------------------------------
-for mname in cf.subkeys('module/*[@type="startup"]'):
-	log.verb("startup module '%s' detected. trying to load..." % mname)
-	try:
-		mod = __import__('modules.%s' % mname, fromlist=["modules"])
-	except ImportError as e:
-		log.error("cannot import module '%s'. ignore." % mname)
-	else:
-		log.info("ok, loaded. starting module '%s'..." % mname)
-		mod.run(cf, comm, log)
+run_blocked_modules('module/*[@type="startup"]')
 
 ### modules for periodic run...	----------------------------------------------
-from threading import Thread
-
-message = {'stop':False}
-modules = list()
-for mname in cf.subkeys('module/*[@type="periodic"]'):
-	log.verb("configration for '%s' detected. trying to load..." % mname)
-	try:
-		mod = __import__('modules.%s' % mname, fromlist=["modules"])
-	except ImportError as e:
-		log.error("cannot import module '%s'. ignore." % mname)
-	else:
-		modules.append({'name':mname, 'module':mod})
-		log.verb("module '%s' registered." % mname)
-
-for mod in modules:
-	log.info("starting module '%s'..." % mod['name'])
-	try:
-		mclass = getattr(mod['module'], mod['name'])
-		mod['thread'] = mclass(cf, comm, log, message)
-		mod['thread'].start()
-	except AttributeError as e:
-		log.error("invalid module '%s'. %s" % (mod['name'], str(e)))
-	except:
-		log.error('unknown exception: %s' % str(e))
-		rais
-
+message = {'stop':False, 'interval':5}
+modules = run_threaded_modules('module/*[@type="periodic"]')
 log.info('now all modules are in running mode...')
-while len(modules):
-	for i in range(len(modules)):
-		t = modules[i]['thread']
-		if not t.is_alive():
-			log.verb('%s already exit.' % t.getName())
-			modules.pop(i)
-			break
-		log.verb('%s alive...' % t.getName())
-		### use signal?
-		try:
-			t.join(2)
-		except KeyboardInterrupt as e:
-			log.info('interrupted! %s' % str(e))
-			message['stop'] = True
 
+
+import threading
+while threading.active_count() > 1:
+	try:
+		for t in modules:
+			if not t.is_alive():
+				log.verb('%s already exit.' % t.getName())
+			else:
+				log.debug('%s alive...' % t.getName())
+				t.join(2)
+	except KeyboardInterrupt as e:
+		log.info('interrupted! stopping...')
+		message['stop'] = True
 
 log.info('all module thread are exit. shutdown myself.')
 
