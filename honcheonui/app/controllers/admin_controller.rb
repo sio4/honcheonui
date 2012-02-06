d2813857794075dee393b6af112ce8a1a3d0aace
Yonghwan SO <sio4@users.sf.net>
2012-02-06 17:29:38 +0900

    add user list view for admin with slickgrid.

--- honcheonui/app/controllers/admin_controller.rb
+++ honcheonui/app/controllers/admin_controller.rb
@@ -1,2 +1,14 @@
 class AdminController < ApplicationController
+	# GET /admin/user
+	# GET /admin/user.json
+	def user
+		@users = User.all
+		@meta = {:total => @users.length}
+
+		respond_to do |format|
+			format.html
+			format.json { render :json => {:meta => @meta, :objs => @users } }
+		end
+	end
 end
+# vim:set ts=4 sw=4:
